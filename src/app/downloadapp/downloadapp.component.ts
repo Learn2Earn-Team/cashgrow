599d7fc7654fb97df2128304f78e8de0acc91b40
Daha Qalbi <qalbichamp5@gmail.com>
2024-03-26 15:08:18 +0500

    updated

--- src/app/downloadapp/downloadapp.component.ts
+++ src/app/downloadapp/downloadapp.component.ts
@@ -1,28 +1,26 @@
-import { Component } from '@angular/core';
-import { ApicallService } from '../services/apicall.service';
-import { check } from '../localStorage/LocalStorage';
+import { Component } from "@angular/core";
+import { ApicallService } from "../services/apicall.service";
+import { check } from "../localStorage/LocalStorage";
 @Component({
-  selector: 'app-downloadapp',
-  templateUrl: './downloadapp.component.html',
-  styleUrls: ['./downloadapp.component.scss']
+  selector: "app-downloadapp",
+  templateUrl: "./downloadapp.component.html",
+  styleUrls: ["./downloadapp.component.scss"],
 })
 export class DownloadappComponent {
   data: any;
   name: any;
+  pageSize = 15;
+  p = 1;
   ngOnInit(): void {
     this.mydailyprofit();
-      }
-      constructor(public apiCall:ApicallService){
-
-      }
-    async  mydailyprofit(){
-        const user: any = await check('user');
-        const userData = JSON.parse(user);
-        this.apiCall.mydailyprofit(userData.username).subscribe((res:any)=>{
-          this.data=res;
-          console.log(this.data);
-        });
-      }
- 
+  }
+  constructor(public apiCall: ApicallService) {}
+  async mydailyprofit() {
+    const user: any = await check("user");
+    const userData = JSON.parse(user);
+    this.apiCall.mydailyprofit(userData.username).subscribe((res: any) => {
+      this.data = res;
+      console.log(this.data);
+    });
+  }
 }
-
